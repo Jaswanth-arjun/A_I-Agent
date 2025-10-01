fb573d1c662a704382ec76e1af5ff5839702fe9c
Jaswanth-arjun <138548908+Jaswanth-arjun@users.noreply.github.com>
2025-10-01 19:14:51 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $host = 'profound-jade-orca-vpmg5-mysql.profound-jade-orca-vpmg5.svc.cluster.local';
 $username = 'mink';
 $password = 'wK2+fH1_wU4=pO4-zJ0_';
-$database = 'userform'; // Changed to match your SQL database
+$database = 'profound-jade-orca'; // Use the original database name
 
 $con = mysqli_connect($host, $username, $password, $database);
 
