5a99c82b4ab1a27952a2228884e49515f134157e
Jaswanth-arjun <138548908+Jaswanth-arjun@users.noreply.github.com>
2025-10-01 18:27:00 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php 
-$con = mysqli_connect('localhost', 'root', '', 'userform');
+$con = mysqli_connect('profound-jade-orca-vpmg5-mysql.profound-jade-orca-vpmg5.svc.cluster.local', 'mink', 'wK2+fH1_wU4=pO4-zJ0_', 'profound-jade-orca');
 ?>
